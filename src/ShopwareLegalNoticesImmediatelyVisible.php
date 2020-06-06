71cfa4955ef1025c4a3bf4f54b507471b5c40d83
Sebastian Volk <volk.sebastian@outlook.de>
2020-06-06 16:53:23 +0200

    Initial commit

---
+++ src/ShopwareLegalNoticesImmediatelyVisible.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareLegalNoticesImmediatelyVisible;
+
+use Shopware\Core\Framework\Plugin;
+
+class ShopwareLegalNoticesImmediatelyVisible extends Plugin
+{
+}
