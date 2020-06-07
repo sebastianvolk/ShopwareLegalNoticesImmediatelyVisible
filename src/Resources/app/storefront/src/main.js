a34ec6b9cfaf3e2e88e1e8b5dc06f0356a2bea2f
Sebastian Volk <volk.sebastian@outlook.de>
2020-06-07 14:08:42 +0200

    Compile plugin

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,31 +1,31 @@
 window.shopwareLegalNoticesImmediatelyVisible = (function () {
-  "use strict";
-  const _openCollapsedFooter = (footerLinkLabel) => {
-    if (footerLinkLabel) {
-      const footerColumns = Array.from(
-        document.getElementsByClassName("footer-column")
-      );
-      footerColumns.forEach((footerColumn) => {
-        if (_footerColumnContainsLabel(footerColumn, footerLinkLabel)) {
-          _toggleCollapsedFooter(footerColumn);
+    'use strict';
+    const _openCollapsedFooter = (footerLinkLabel) => {
+        if (footerLinkLabel) {
+            const footerColumns = Array.from(
+                document.getElementsByClassName('footer-column')
+            );
+            footerColumns.forEach((footerColumn) => {
+                if (_footerColumnContainsLabel(footerColumn, footerLinkLabel)) {
+                    _toggleCollapsedFooter(footerColumn);
+                }
+            });
         }
-      });
-    }
-  };
-  const _footerColumnContainsLabel = (footerColumn, footerLinkLabel) => {
-    const footerLinkItems = Array.from(
-      footerColumn.getElementsByClassName("footer-link-item")
-    ).map((linkItem) =>
-      linkItem.getElementsByTagName("a")[0].getAttribute("title")
-    );
-    return footerLinkItems.includes(footerLinkLabel);
-  };
-  const _toggleCollapsedFooter = (footerColumn) => {
-    footerColumn
-      .getElementsByClassName("js-collapse-footer-column-trigger")[0]
-      .click();
-  };
-  return {
-    openCollapsedFooter: _openCollapsedFooter,
-  };
+    };
+    const _footerColumnContainsLabel = (footerColumn, footerLinkLabel) => {
+        const footerLinkItems = Array.from(
+            footerColumn.getElementsByClassName('footer-link-item')
+        ).map((linkItem) =>
+            linkItem.getElementsByTagName('a')[0].getAttribute('title')
+        );
+        return footerLinkItems.includes(footerLinkLabel);
+    };
+    const _toggleCollapsedFooter = (footerColumn) => {
+        footerColumn
+            .getElementsByClassName('js-collapse-footer-column-trigger')[0]
+            .click();
+    };
+    return {
+        openCollapsedFooter: _openCollapsedFooter,
+    };
 })();
