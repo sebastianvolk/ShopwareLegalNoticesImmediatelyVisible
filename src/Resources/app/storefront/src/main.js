c6a43a699c9c0eed04b4e5464c30a7db3bcbd35d
Sebastian Volk <volk.sebastian@outlook.de>
2020-06-07 12:20:26 +0200

    Wait until DOM content is loaded

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,9 @@
 window.shopwareLegalNoticesImmediatelyVisible = (function () {
-    'use strict';
-    const _openCollapsedFooter = (footerLinkLabel) => {
-        console.log('Hello World!', footerLinkLabel);
-    };
-    return {
-        openCollapsedFooter: _openCollapsedFooter,
-    };
+  "use strict";
+  const _openCollapsedFooter = (footerLinkLabel) => {
+    console.log("Hello World!", footerLinkLabel);
+  };
+  return {
+    openCollapsedFooter: _openCollapsedFooter,
+  };
 })();
