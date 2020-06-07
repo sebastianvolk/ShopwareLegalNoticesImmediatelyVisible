fdcb8eca9e7af73cf266b8b9bc242615167534cf
Sebastian Volk <volk.sebastian@outlook.de>
2020-06-07 13:41:34 +0200

    Add ability to search for label

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,7 +1,20 @@
 window.shopwareLegalNoticesImmediatelyVisible = (function () {
   "use strict";
   const _openCollapsedFooter = (footerLinkLabel) => {
-    console.log("Hello World!", footerLinkLabel);
+    if (footerLinkLabel) {
+      const footerColumns = document.getElementsByClassName("footer-column");
+      footerColumns.forEach((footerColumn) => {
+        console.log(_footerColumnContainsLabel(footerColumn, footerLinkLabel));
+      });
+    }
+  };
+  const _footerColumnContainsLabel = (footerColumn, footerLinkLabel) => {
+    const footerLinkItems = Array.from(
+      footerColumn.getElementsByClassName("footer-link-item")
+    ).map((linkItem) =>
+      linkItem.getElementsByTagName("a")[0].getAttribute("title")
+    );
+    return footerLinkItems.includes(footerLinkLabel);
   };
   return {
     openCollapsedFooter: _openCollapsedFooter,
