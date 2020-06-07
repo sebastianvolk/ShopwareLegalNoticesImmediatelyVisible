1a038eccc32ed34f2520fc5daa140a0e8d62bbf1
Sebastian Volk <volk.sebastian@outlook.de>
2020-06-07 13:53:22 +0200

    Fix forEach loop

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,22 +1,24 @@
 window.shopwareLegalNoticesImmediatelyVisible = (function () {
-    'use strict';
-    const _openCollapsedFooter = (footerLinkLabel) => {
-        if (footerLinkLabel) {
-            const footerColumns = document.getElementsByClassName('footer-column');
-            footerColumns.forEach((footerColumn) => {
-                console.log(_footerColumnContainsLabel(footerColumn, footerLinkLabel));
-            });
-        }
-    };
-    const _footerColumnContainsLabel = (footerColumn, footerLinkLabel) => {
-        const footerLinkItems = Array.from(
-            footerColumn.getElementsByClassName('footer-link-item')
-        ).map((linkItem) =>
-            linkItem.getElementsByTagName('a')[0].getAttribute('title')
-        );
-        return footerLinkItems.includes(footerLinkLabel);
-    };
-    return {
-        openCollapsedFooter: _openCollapsedFooter,
-    };
+  "use strict";
+  const _openCollapsedFooter = (footerLinkLabel) => {
+    if (footerLinkLabel) {
+      const footerColumns = Array.from(
+        document.getElementsByClassName("footer-column")
+      );
+      footerColumns.forEach((footerColumn) => {
+        console.log(_footerColumnContainsLabel(footerColumn, footerLinkLabel));
+      });
+    }
+  };
+  const _footerColumnContainsLabel = (footerColumn, footerLinkLabel) => {
+    const footerLinkItems = Array.from(
+      footerColumn.getElementsByClassName("footer-link-item")
+    ).map((linkItem) =>
+      linkItem.getElementsByTagName("a")[0].getAttribute("title")
+    );
+    return footerLinkItems.includes(footerLinkLabel);
+  };
+  return {
+    openCollapsedFooter: _openCollapsedFooter,
+  };
 })();
